68fc5afa19e0debb60f29ff5524268f3894e5d17
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2024-06-03 23:30:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,57 @@
 const { createServer } = require('http');
-const { Gtc } = require('gtc-js')
-const qrcode = require('qrcode-terminal')
+const PN = require("awesome-phonenumber")
 const express = require('express');
 const path = require('path'); 
 const app = express();
-let anu;
+const truecallerjs = require("truecallerjs");
+const axios = require('axios');
 
 app.get("/", (req, res) => {
-  res.send("denis kontol")
+  res.send("GET /lookup?no=")
 })
-app.get("/gtc", async(req, res) => {
-  let { nomer } = req.query
-  main(nomer).then(() => {
-    res.send(anu)
-  })
+app.get("/lookup", async(req, res) => {
+  let { no } = req.query
+  let truecaller = await tcl(no)
+  let eyecon = await eyecon(no)
+  res.json({ truecaller, eyecon })
 })
 
-const main = async (nomer) => {
-  const gtc = new Gtc({
-    cookiePath: './cookie.json',
-    showQr: false,
-    puppeteer: {
-      headless: false,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      ignoreDefaultArgs: ['--disable-extensions'],
-    },
-  })
+const tcl = async (no) => {
+    const searchData = {
+        number: no,
+        countryCode: await (await getCountry(no)).region,
+        installationId: "a2i0Z--nBHL5ckIkLhqh1dIziCjrZApo0sZyWzPHJQkz12jc6VSV7Y4r27Hufp6l",
+    };
 
-  gtc.on('qrcode', (value) => {
-    qrcode.generate(value, { small: true })
-  })
+    return await truecallerjs.search(searchData)
+        .then(async response => {
+            return response.json()
+        });
+};
 
-  gtc.on('logged', (logged) => {
-    if (logged) {
-      console.log('logged')
-    } else {
-      console.log('scan qr code first')
-    }
-  })
-
-  gtc.on('error', (error) => {
-    console.error(error)
-  })
-
-  await gtc.init()
-
-  anu = await gtc.find('ID', nomer) // just random phone number
-  
+const getCountry = async(no) => {
+let { regionCode, countryCode } = await (await PN("+"+no)).g
+return { region: regionCode, country: countryCode }
 }
+const eyecon = async (no) => {
+    const { country } = await getCountry(no)
+    const config = {
+        method: 'get',
+        url: `https://eyecon.p.rapidapi.com/api/v1/search?code=${country}&number=${no.substring(2)}`,
+        headers: {
+            'x-rapidapi-host': 'eyecon.p.rapidapi.com',
+            'x-rapidapi-key': '8fbd6b3e85msh018cc5b36b3ec24p1da66fjsn3a5a1073ba31'
+        }
+    };
+
+    try {
+        const response = await axios(config);
+        return response.data;
+    } catch (error) {
+        console.error('Eyecon API error:', error);
+        throw error;
+    }
+};
 
 const server = createServer(app);
 
